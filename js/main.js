dc608a9c61e2d8a5f074ddd1f8b3cfef59443a11
Misha Ops <101456465+misha81@users.noreply.github.com>
2023-06-15 16:09:26 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// ====1====
+const result= 5+5+"5";
+console.log(result)
+console.log(typeof(result))
+// ====2====
+const email = "MishaOps81@gmail.com";
+console.log(email.includes("@"))
+console.log(email.length)
+// ====3====
+const my = "My";
+const name = "name";
+const is = "is";
+const fullname = my + " " + name + " " + is;
+const Viktor = " Viktor";
+console.log(fullname + Viktor)
+// ====4====
+const userName = "Максим";
+const payment = 500;
+const string = "Дякуємо, " + userName + "!" + " До сплати " + payment + " гривень";
+console.log(string)
